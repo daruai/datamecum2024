41af2a54a6b30b7fde170f6f8d41a5520898636c
Daniel Ruiz <danielruizriquelme@gmail.com>
2024-07-27 11:15:00 +0200

    Añado funcionalidad de split train-test

--- src/mlops/ingest_data/ingest_data.py
+++ src/mlops/ingest_data/ingest_data.py
@@ -6,3 +6,7 @@ def get_data():
     data = datasets.load_iris()
     X, y = data.data, data.target
     return X, y
+
+def split_train_test(X, y, test_size=0.2):
+    from sklearn.model_selection import train_test_split
+    return train_test_split(X, y, test_size=test_size, random_state=42)
